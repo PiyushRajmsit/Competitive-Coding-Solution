850667c1a134d4bc838c275f8708ddc0b072bc48
Piyush Raj <piyraj007@gmail.com>
2019-12-30 20:24:13 +0530

    MicroandCoins.cpp added

---
+++ Hackerearth/MicroandCoins.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+		int adj[20][20] = {0};
+		int n,m;
+		cin>>n>>m;
+		for (int i = 0; i < m; ++i)
+		{
+			int x,y;
+			cin>>x>>y;
+
+			adj[x-1][y-1] = 1;
+			adj[y-1][x-1] = 1;
+		}
+
+		bool **dp = new bool*[n];
+
+		for (int i = 0; i < n; ++i)
+		{
+			dp[i] = new bool[1<<n];
+			for (int j = 0; j < (1<<n);j++)
+			{
+				dp[i][j] = false;
+			}
+		}
+		for (int i = 0; i < n; ++i)
+		{
+		dp[i][1<<i] = true;
+		}
+		for (int i = 0; i < (1<<n); ++i)
+		{
+			for(int j=0;j<n;j++)
+			{
+				int bit = i&(1<<j);
+				if(bit != 0)
+				{
+					for(int k=0;k<n;k++)
+					{
+						int also = i&(1<<k);
+					
+						if(also != 0 && j!= k && adj[j][k] == 1)
+						{
+							if(dp[k][i^(1<<j)] == true)
+							{
+								dp[j][i] = true;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		bool flag = false;
+		for (int i = 0; i < n; ++i)
+		{
+			if(dp[i][(1<<n) - 1])
+			{
+				flag = true;
+				break;
+			}
+		}
+		if(flag)
+		{
+			cout<<"Yes"<<endl;
+		}
+		else{
+			cout<<"No"<<endl;
+		}
+
+		for (int i = 0; i < n; ++i)
+			delete[] dp[i];
+
+		delete[] dp;
+    }
+	return 0;
+}
